549b60327c4fd88146629f908b43ed18bbe01c25
Felipe Amorim <felipe@ive.local>
2025-07-15 14:23:25 -0300

    Corrigir configuração de arquivos estáticos do Django admin

--- crm_empresa/urls.py
+++ crm_empresa/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from registros.views import RegistroViewSet
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'registros', RegistroViewSet)
@@ -28,3 +30,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
